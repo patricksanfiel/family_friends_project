fb884828c4cd0603db9247a28d6515b3f0ec853d
Kevin Schaffter <kevinschaffter@gmail.com>
2017-11-24 15:45:17 -0500

    Fixed email validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true
+  validates :email, presence: true
 
 end

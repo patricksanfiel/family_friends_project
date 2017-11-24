f81118d841c06101506af7bbf98a19fa45e9c75f
Kevin Schaffter <kevinschaffter@gmail.com>
2017-11-24 12:54:21 -0500

    Added login/logout to index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :products, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+
 end
